18569acd04a4f15248fd8c5629fab5744feea5c7
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-05-07 16:18:14 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+module.exports.animeData = [
+  {
+    id: 1,
+    name: 'Jujutsu Kaisen',
+    seasons: [
+      {
+        id: 1,
+        name: 'Season 1',
+        episodes: [
+          { id: 1, name: 'Episode 1', videoLink: 'https://link.to/jujutsu-episode1' },
+          { id: 2, name: 'Episode 2', videoLink: 'https://link.to/jujutsu-episode2' },
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Demon Slayer',
+    seasons: [
+      {
+        id: 1,
+        name: 'Season 1',
+        episodes: [
+          { id: 1, name: 'Episode 1', videoLink: 'https://link.to/demon-slayer-episode1' },
+          { id: 2, name: 'Episode 2', videoLink: 'https://link.to/demon-slayer-episode2' },
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Chainsaw Man',
+    seasons: [
+      {
+        id: 1,
+        name: 'Season 1',
+        episodes: [
+          { id: 1, name: 'Episode 1', videoLink: 'https://link.to/chainsaw-man-episode1' },
+          { id: 2, name: 'Episode 2', videoLink: 'https://link.to/chainsaw-man-episode2' },
+        ]
+      }
+    ]
+  }
+];
