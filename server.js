236867c34df4ccd2cc4302090aee51cb90e62d80
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-05-07 22:52:40 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-
-const animeRoutes = require("./routes/anime");
+const express = require('express');
+const cors = require('cors');
+const animeRoutes = require('./routes/anime');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/anime", animeRoutes);
+app.use('/api/anime', animeRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Anime API running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('Anime Hindi API Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
