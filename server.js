aee2777735945f8cb0e9357b5de0b4aa1e6ba25e
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-05-07 20:26:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const app = express();
-const animeRoutes = require('./routes/anime');
+const express = require("express");
+const cors = require("cors");
 
-app.use('/api/anime', animeRoutes);
-app.get('/', (req, res) => res.send("AnimeHindi API Running"));
+const animeRoutes = require("./routes/anime");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/anime", animeRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Anime API running on port ${PORT}`));
